9fc797531946b99ec3b2b9fe87cc919ad2108983
PalakMishraGit <amritraj7461@gmail.com>
2026-05-01 17:40:49 +0530

    All txt files added

--- searchRoutes.js
+++ searchRoutes.js
@@ -1,15 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const { MongoClient } = require("mongodb");
+
+const uri = "mongodb://localhost:27017";
 
 router.get("/api/search", async (req, res) => {
-  const category = req.query.category;
+  try {
+    const category = req.query.category;
 
-  const items = await req.app.locals.db
-    .collection("items")
-    .find({ category: category })
-    .toArray();
+    const client = new MongoClient(uri);
+    await client.connect();
 
-  res.json(items);
+    const db = client.db("giftlink");
+
+    const items = await db
+      .collection("items")
+      .find({ category: category })
+      .toArray();
+
+    res.json(items);
+
+    await client.close(); // optional but good practice
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 module.exports = router;
